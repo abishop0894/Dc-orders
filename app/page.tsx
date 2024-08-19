e91f52cd4fd3b5992e1e38bf5a097ec8d9490de3
Alexander Bishop <abishop0894@gmail.com>
2024-08-19 16:58:04 -0400

    new

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,6 @@ import Orders from "@/modules/orders";
 import { Providers } from "./providers";
 import DadcraftingCMS from "@/modules/cms";
 import Login from "@/modules/login";
-export default function Page() {
+export default function App() {
   return <Login />;
 }
